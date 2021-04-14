b71e2169b607d4ea2d6324b872136331b39e3596
mahmoudkhader <>
2021-04-14 12:00:33 +0000

    lab 04

---
+++ app.js
@@ -0,0 +1,33 @@
+alert('Welcome to my website');
+
+var yourAage= prompt('please enter your age');
+
+if (yourAage >=50 ){
+document.write('you are too old');
+document.write('<img src="https://thumbs.dreamstime.com/z/old-elderly-male-adult-confused-old-person-108512602.jpg">');
+}else if(yourAage >=18){
+document.write('you are young');
+document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8Zz4FGRCL69kDU-kehtc8JqGaBPVnNF9oxw&usqp=CAU">');
+}else if(yourAage >=0){
+  document.write('you are a baby');
+  document.write('<img src="https://static.boredpanda.com/blog/wp-content/uploads/2019/07/baby-portraits-teeth-added-coffee-creek-studio-amy-haehl-fb-png__700.jpg">');
+}
+var number=10;
+console.log(number);
+
+if(number!=='10'){
+  console.log('true')
+}else{
+  console.log('false')
+}
+
+var x=5
+var y=10
+
+if(x*y==30){
+  console.log('its true',50);
+}else {
+  console.log('its false',30);
+}
+
+
